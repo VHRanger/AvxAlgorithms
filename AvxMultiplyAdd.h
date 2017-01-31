52c0b6d09f258d8943197144089b1445d545e260
VHRanger <matthieu.ranger@outlook.com>
2017-01-30 22:58:16 -0500

    Update AvxMultiplyAdd.h

--- AvxMultiplyAdd.h
+++ AvxMultiplyAdd.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <immintrin.h>
 
+using std::vector;
+
 //multiply a with b, add result to result
 //Requires a vector with length divisible by 8 (for chunking).
 //As such, make sure to pad vectors whose v.size()%8 != 0 such that it is the case.
